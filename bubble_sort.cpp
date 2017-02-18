58e66662c1647d2e13915302572273ee4e055b01
Josh Raynard Cleofas De Vera <junixdevera@yahoo.com>
2017-02-18 11:55:44 +0800

    Create bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,34 @@
+//JOSH DV
+#include<iostream>
+using namespace std;
+
+int main(){
+	int A1[5]={35,10,55,20,5};
+	cout<<"BUBBLE SORTING:\n";
+	for(int i=1;i<5;i++){
+		for(int j=0;j<4;j++){
+			if(A1[j]>A1[j+1]){
+				int temp = A1[j];
+				A1[j] = A1[j+1];
+				A1[j+1] = temp;
+			}
+		}
+		if(i==1){
+			cout<<"1st Pass: ";
+		}
+		if(i==2){
+			cout<<"2nd Pass: ";
+		}
+		if(i==3){
+			cout<<"3rd Pass: ";
+		}
+		if(i>3){
+			cout<<i<<"th Pass: ";
+		}
+		for(int i=0;i<5;i++){
+			cout<<A1[i] << " ";
+		}
+		cout<<"\n\n";
+	}
+	return 0;
+}
