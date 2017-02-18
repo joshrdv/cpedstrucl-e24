71cae26d85e9e3819ca53e17e1390111219feb9a
Josh Raynard Cleofas De Vera <junixdevera@yahoo.com>
2017-02-18 12:32:38 +0800

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,16 +1,19 @@
-//JOSH DV
 #include<iostream>
 using namespace std;
 
 int main(){
 	int A1[5]={35,10,55,20,5};
+	int p,q;
 	cout<<"BUBBLE SORTING:\n";
 	for(int i=1;i<5;i++){
 		for(int j=0;j<4;j++){
 			if(A1[j]>A1[j+1]){
 				int temp = A1[j];
+				q = A1[j];
 				A1[j] = A1[j+1];
+				p = A1[j];
 				A1[j+1] = temp;
+				cout<<"Swapped: "<<p<<", "<<q<<"\n";
 			}
 		}
 		if(i==1){
