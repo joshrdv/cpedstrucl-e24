0bd21b4b93f2a317f8885a93882a96ea9119657a
Josh Raynard Cleofas De Vera <junixdevera@yahoo.com>
2017-02-18 11:57:01 +0800

    Create insertion_sort.cpp

---
+++ insertion_sort.cpp
@@ -0,0 +1,31 @@
+//JOSH DV
+#include<iostream>
+using namespace std;
+
+int main(){
+	int A1[5]={40,30,20,50,10};
+	cout<<"INSERTION SORTING:\n";
+	for(int i=1;i<5;i++){
+		int j,tmp = A1[i];
+		for(j=i-1;j>=0 && tmp<A1[j];j--){
+			A1[j+1] = A1[j];
+		}
+		A1[j+1] = tmp;
+		if(i==1){
+			cout<<"1st Pass: ";
+		}
+		if(i==2){
+			cout<<"2nd Pass: ";
+		}
+		if(i==3){
+			cout<<"3rd Pass: ";
+		}
+		if(i>3){
+			cout<<i<<"th Pass: ";
+		}
+		for(int k=0;k<5;k++){
+			cout<<A1[k]<<" ";
+		}
+		cout<<"\n\n";
+	}	
+}
