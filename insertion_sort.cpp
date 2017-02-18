690405019a20588a4361cf77f168657de319cda4
Josh Raynard Cleofas De Vera <junixdevera@yahoo.com>
2017-02-18 12:42:27 +0800

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -5,12 +5,16 @@ using namespace std;
 int main(){
 	int A1[5]={40,30,20,50,10};
 	cout<<"INSERTION SORTING:\n";
+	int p,q;
 	for(int i=1;i<5;i++){
 		int j,tmp = A1[i];
 		for(j=i-1;j>=0 && tmp<A1[j];j--){
 			A1[j+1] = A1[j];
+			p = A1[j];
+			q=tmp;
 		}
 		A1[j+1] = tmp;
+		cout<<"Swapped: "<<p<<", "<<q<<"\n";
 		if(i==1){
 			cout<<"1st Pass: ";
 		}
